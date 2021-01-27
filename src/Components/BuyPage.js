8c7ffb126fe37070885691fd56d7a7b994cc98ed
Ganesh Kale <kaleg2025@gmail.com>
2021-01-27 16:51:24 +0530

    Method allProduct added to map data

--- src/Components/BuyPage.js
+++ src/Components/BuyPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Axios } from "axios";
+import Axios from "axios";
 import { random, commerce } from "faker";
 import { Container, Col, Row } from "reactstrap";
 
@@ -7,31 +7,49 @@ const apiKey = "INSERT_YOUR_API_KEY";
 
 const url = "https://api.pexels.com/v1/search?query=laptop&per_page=6&page=1";
 
-const localurl = "https://jsonware.com/json/7f26bf2c0233a09ad8426b4e6ad9ccbd.json";
+const localurl =
+  "https://jsonware.com/json/7f26bf2c0233a09ad8426b4e6ad9ccbd.json";
 
 const BuyPage = ({ addInCart }) => {
+  const [product, setProduct] = useState([]);
 
-    const [product, setProduct] = useState([]);
+  // const fetchPhotos = async () => {
+  //     const response = await Axios.get(url, {
+  //         header: {
+  //             Authorization: apiKey
+  //         }
+  //     });
+  // }
 
-    // const fetchPhotos = async () => {
-    //     const response = await Axios.get(url, {
-    //         header: {
-    //             Authorization: apiKey
-    //         }
-    //     });
-    // }
-
-    const fetchPhotos = async () => {
-        const { data } = await Axios.get(localurl);
-    };
+  const fetchPhotos = async () => {
+    const { data } = await Axios.get(localurl);
 
     const { photos } = data;
-    
-    useEffect(() => {
-        fetchPhotos();
-    }, [])
- 
-    return (
-        //TODO:
-    );
+
+    const allProduct = photos.map((photo) => ({
+      smallImage: photo.src.medium,
+      timyImage: photo.src.tiny,
+      productName: random.word(),
+      productPrice: commerce.price(),
+      id: random.uuid(),
+    }));
+
+    setProduct(allProduct);
+  };
+
+  useEffect(() => {
+    fetchPhotos();
+  }, []);
+
+  return (
+    <Container fluid>
+      <h1 className="text-success">Buy page</h1>
+      <Row>
+        {product.map((product) => (
+          <span key={product.id}>{product.productName}</span>
+        ))}
+      </Row>
+    </Container>
+  );
 };
+export default BuyPage;
