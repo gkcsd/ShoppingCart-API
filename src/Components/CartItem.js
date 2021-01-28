87cc61360ed0eb1aa317de48189c2d7d8bd462f5
Ganesh Kale <kaleg2025@gmail.com>
2021-01-28 14:44:27 +0530

    Button Syntax Modified

--- src/Components/CartItem.js
+++ src/Components/CartItem.js
@@ -11,13 +11,18 @@ import {
 const CartItem = ({ product, addInCart }) => {
   return (
     <Card className="mt-2 mb-1">
-      <CardImg top height="250" width="100%" src={product.smallImage} />
+      <CardImg top height="200" width="100%" src={product.smallImage} />
       <CardBody className="text-center">
         <CardTitle>{product.productName}</CardTitle>
         <CardText className="secondary">
           Price: $ {product.productPrice}
         </CardText>
-        <Button color="success" onClick={() => addInCart(product)}>
+        <Button
+          color="success"
+          onClick={() => {
+            addInCart(product);
+          }}
+        >
           Buy Now
         </Button>
       </CardBody>
